cbc3963e0db126c40ba3312ab108dc5fcb72d193
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-22 20:01:03 +0500

    add test_redirect_for_anonymous_client

--- notes/tests/tests_routes.py
+++ notes/tests/tests_routes.py
@@ -56,4 +56,16 @@ class TestRoutes(TestCase):
                     self.assertEqual(response.status_code, status)
 
     def test_redirect_for_anonymous_client(self):
-        
+        login_url = reverse('users:login')
+        urls = (
+            'notes:detail',
+            'notes:edit',
+            'notes:delete',
+            # 'notes:list'
+        )
+        for name in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.note.slug,))
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
