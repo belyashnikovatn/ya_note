d74c5212f964881688a3524d09b89cdd18277351
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-24 20:12:13 +0500

    add more test_routes

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -6,6 +6,16 @@ from django.urls import reverse
 
 @pytest.mark.parametrize(
     'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
     ('notes:list', 'notes:add', 'notes:success')
 )
 def test_pages_availability_for_auth_user(not_author_client, name):
@@ -22,3 +32,22 @@ def test_pages_availability_for_author(author_client, name, note):
     url = reverse(name, args=(note.slug,))
     response = author_client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expexted_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete')
+)
+def test_pages_availability_for_different_users(
+    parametrized_client, name, note, expexted_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expexted_status
